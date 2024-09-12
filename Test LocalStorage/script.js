0bd9bce5f3f83e7d1faf870826ba4c10867d9725
Arthur Marques <162700725+ArthurLux07@users.noreply.github.com>
2024-09-12 11:33:02 -0300

    Add files via upload

---
+++ Test LocalStorage/script.js
@@ -0,0 +1,14 @@
+function bora(){
+
+    const bglDoInput = document.getElementById('valor').value
+
+    localStorage.setItem('Número', bglDoInput)
+
+    console.log('Seu número já foi armazenado!')
+}
+
+
+
+
+
+
