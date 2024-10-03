86f558b8f286cd1b01c0dc47d6a1a6e6ec11b10b
Arthur Marques <162700725+ArthurLux07@users.noreply.github.com>
2024-10-03 11:17:27 -0300

    Add files via upload

---
+++ IF ELSE ARRAY/Ex_1.js
@@ -0,0 +1,8 @@
+//Escreva uma função chamada verificarParidade que receba um número inteiro como argumento e retorne uma string indicando se o número é "Par" ou "Ímpar"./*
+
+function verificarParidade 
+
+    
+
+   
+
---
+++ IF ELSE ARRAY/Ex_2.js
@@ -0,0 +1,17 @@
+//Crie uma função chamada idadeParaAposentadoria que receba a idade de uma pessoa e retorne uma mensagem indicando se ela já pode se aposentar ou ainda precisa trabalhar. Considere que a idade mínima para aposentadoria é 65 anos.
+
+var idadeParaAposentadoria
+
+prompt('Quantos anos você tem?')
+
+if(idadeParaAposentadoria >= 65){
+
+    console.log('Você já pode se aposeantar! :)')
+
+} else{
+
+    alert('Você ainda não pode se aposentar! :(')
+}
+
+
+
